da3a9a82b5de8afc27e80ee143e9f957bb2b1611
wisatong <wisa.pow@gmail.com>
2019-12-12 02:08:26 +0700

    finish setup babel and webpack

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import {} from './app.css';
+
+const App = () => {
+  return (
+    <div class="app">
+      <h1>Preact is cool</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/client/index.js"
+  , mode: "development"
+  , module: {
+    rules: [
+      { test: /\.js$/, loader: "babel-loader", include: path.join(__dirname, 'src', 'client')}
+      , { test: /\.css$/, loaders: ["style-loader", "css-loader"], include: path.join(__dirname, 'src', 'client')}
+    ]
+  }
+  , resolve: { extensions: ["*", ".js", ".jsx"] }
+  , output: {
+    path: path.resolve(__dirname, "dist/")
+    , publicPath: "/dist/"
+    , filename: "bundle.js"
+  }
+  , devServer: {
+    contentBase: path.join(__dirname, "public/")
+    , port: 3000
+    , publicPath: "http://localhost:3000/dist/"
+    , hotOnly: true
+    , historyApiFallback: true
+  }
+  , plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
