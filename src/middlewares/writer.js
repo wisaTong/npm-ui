e724252da5f5e557ad42601a0d3c63f82df587a3
wisatong <wisa.pow@gmail.com>
2019-12-13 04:40:39 +0700

    added install uninstall

---
+++ src/middlewares/writer.js
@@ -0,0 +1,37 @@
+import { execSync } from 'child_process';
+
+const getInstallCommand = (pkg, dev) => {
+  let command = `npm install ${pkg} --save`;
+  return dev ? command + '-dev' : command;
+}
+
+const install = (req, res, next) => {
+  const { path, pkg, dev } = req.body;
+
+  const command = getInstallCommand(pkg, dev);
+
+  process.chdir(path);
+  try {
+    execSync(command, { encoding: 'utf-8' });
+  } catch (error) {
+    return res.error(error)
+  }
+
+  return res.end();
+}
+
+const uninstall = (req, res, next) => {
+  const { path, pkg } = req.body;
+  const command = `npm uninstall ${pkg} --save`;
+
+  process.chdir(path);
+  try{
+    execSync(command, { encoding: 'utf-8'});
+  } catch (error) {
+    return res.error(error);
+  }
+
+  return res.end();
+}
+
+export { install, uninstall };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { readDirs, exists, readFile } from './middlewares/fsreader';
+import { readDirs, exists, readFile } from './middlewares/reader';
+import { install, uninstall } from './middlewares/writer';
 
 const router = Router();
 
 router.get('/dirs', readDirs);
 router.get('/exists', exists);
 router.get('/readfile', readFile);
+
+router.post('/install', install);
+router.delete('/uninstall', uninstall);
 
 export default router;
