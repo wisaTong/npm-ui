030e7b0fec9d5a09eea662e80bff52251d10d1d2
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-12-27 20:02:30 +0700

    Added UI for editing and deleting script

--- src/client/components/ScriptList/presentational.js
+++ src/client/components/ScriptList/presentational.js
@@ -1,10 +1,36 @@
 import _ from "underscore";
 import { h, Fragment } from "preact";
 import "./style.css";
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 const ScriptEntry = ({ name, command }) => {
-  return <div class="script-entry">{name}</div>;
+  const [editing, setEditing] = useState(false);
+  const scriptEditedHandler = () => {};
+  return (
+    <div class="script-entry">
+      <div class="script-label">
+        {editing ? (
+          <div class="script-edit">
+            <input type="text" placeholder="Script Name" value={name} />
+          </div>
+        ) : (
+          <span>{name}</span>
+        )}
+      </div>
+      <div class="script-actions">
+        {editing ? (
+          <button class="script-action" onClick={scriptEditedHandler}>
+            Done
+          </button>
+        ) : (
+          <button class="script-action" onClick={() => setEditing(true)}>
+            Edit
+          </button>
+        )}
+        <button class="script-action">Delete</button>
+      </div>
+    </div>
+  );
 };
 
 const ScriptList = ({ pkgJson, path }) => {
