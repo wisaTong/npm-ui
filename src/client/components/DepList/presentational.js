b9a5bc7afb2affd33fff667bc78337ef23320f05
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-12-27 14:17:43 +0700

    Typo and Styling Edited

--- src/client/components/DepList/presentational.js
+++ src/client/components/DepList/presentational.js
@@ -1,22 +1,28 @@
-import _ from 'underscore';
-import { h, Fragment } from 'preact';
-import { useState } from 'preact/hooks';
-import api from '../../api';
-import './style.css';
+import _ from "underscore";
+import { h, Fragment } from "preact";
+import { useState } from "preact/hooks";
+import api from "../../api";
+import "./style.css";
 
 const DepList = ({ pkgJson, path }) => {
   const { dependencies, devDependencies } = pkgJson;
   return (
-    <div class='deplist-holder'>
-      <h1>Dependecies</h1>
-      <ul class='deplist'>
-        {_.keys(dependencies)
-          .map(d => <DepEntry name={d} callback={() => api.uninstall(path, d)} />)}
+    <div class="deplist-holder">
+      <h1>Dependencies</h1>
+      <ul class="deplist">
+        {_.keys(dependencies).map(d => (
+          <DepEntry name={d} callback={() => api.uninstall(path, d)} />
+        ))}
       </ul>
-      <h1>Development Dependecies</h1>
-      <ul class='deplist'>
-        {_.keys(devDependencies)
-          .map(d => <DepEntry name={d} callback={() => api.uninstall(path, d)} dev={true} />)}
+      <h1>Development Dependencies</h1>
+      <ul class="deplist">
+        {_.keys(devDependencies).map(d => (
+          <DepEntry
+            name={d}
+            callback={() => api.uninstall(path, d)}
+            dev={true}
+          />
+        ))}
       </ul>
     </div>
   );
@@ -36,20 +42,29 @@ const DepEntry = ({ name, callback, dev }) => {
   const mouseOver = () => setHover(true);
   const mouseLeave = () => setHover(false);
 
-  return (
-    gone
-      ? null
-      : <li class= {dev ? 'list depdev-name' : 'list dep-name' }onMouseOver={mouseOver} onMouseLeave={mouseLeave}>
-        {uninstalling
-          ? <div class='spinner'></div>
-          : <Fragment>
-            {name}
-            {hover ? <span class='uninstall' onClick={() => uninstall(setUninstalling, setGone, callback)}>uninstall</span> : null}
-        </Fragment>}
-      </li>
+  return gone ? null : (
+    <li
+      class={dev ? "list depdev-name" : "list dep-name"}
+      onMouseOver={mouseOver}
+      onMouseLeave={mouseLeave}
+    >
+      {uninstalling ? (
+        <div class="spinner"></div>
+      ) : (
+        <Fragment>
+          {name}
+          {hover ? (
+            <span
+              class="uninstall"
+              onClick={() => uninstall(setUninstalling, setGone, callback)}
+            >
+              uninstall
+            </span>
+          ) : null}
+        </Fragment>
+      )}
+    </li>
   );
 };
-
-
 
 export default DepList;
