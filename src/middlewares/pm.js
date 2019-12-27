d960ee4ec6f50af31975aee623692811a3605334
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-12-27 14:32:04 +0700

    Custom Project Name Configuration Added

--- src/middlewares/pm.js
+++ src/middlewares/pm.js
@@ -1,36 +1,42 @@
-import Datastore from 'nedb-promises'
-import { exec } from 'child_process'
-const datastore = Datastore.create('pm.db')
+import Datastore from "nedb-promises";
+import { exec } from "child_process";
+import { writeFileSync } from "fs";
+const datastore = Datastore.create("pm.db");
 
 export const getProjects = async (req, res, next) => {
-  const data = await datastore.find()
-  res.json(data)
-}
+  const data = await datastore.find();
+  res.json(data);
+};
 
 export const newProject = async (req, res, next) => {
-  const {name, path} = req.body;
+  const { name, path } = req.body;
   try {
     await datastore.insert({
       name,
       path
     });
-    exec('npm init -y', {cwd: path},(err, stdout, stderr) => {
+    exec("npm init -y", { cwd: path }, (err, stdout, stderr) => {
       if (err) {
         return;
       }
-      res.sendStatus(201)
-    })
+      const pkgJSON = require(`${path}/package.json`);
+      pkgJSON.name = name;
+      writeFileSync(`${path}/package.json`, JSON.stringify(pkgJSON), {
+        encoding: "utf8"
+      });
+      res.sendStatus(201);
+    });
   } catch (err) {
-    res.sendStatus(400)
-  } 
-}
+    res.sendStatus(400);
+  }
+};
 
 export const removeProject = async (req, res, next) => {
-  const {name} = req.body;
+  const { name } = req.body;
   try {
-    await datastore.remove({name})
-    res.sendStatus(200)
+    await datastore.remove({ name });
+    res.sendStatus(200);
   } catch (err) {
-    res.sendStatus(400)
+    res.sendStatus(400);
   }
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/client/index.js"]
-  , mode: "development"
-  , module: {
+  entry: ["@babel/polyfill", "./src/client/index.js"],
+  mode: "development",
+  module: {
     rules: [
-      { test: /\.js$/, loader: "babel-loader", include: path.join(__dirname, 'src', 'client')}
-      , { test: /\.css$/, loaders: ["style-loader", "css-loader"], include: path.join(__dirname, 'src', 'client')}
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: path.join(__dirname, "src", "client")
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+        include: path.join(__dirname, "src", "client")
+      }
     ]
-  }
-  , resolve: { extensions: ["*", ".js", ".jsx"] }
-  , output: {
-    path: path.resolve(__dirname, "dist/")
-    , publicPath: "/dist/"
-    , filename: "bundle.js"
-  }
-  , devServer: {
-    contentBase: path.join(__dirname, "public/")
-    , port: 8080
-    , publicPath: "http://localhost:3000/"
-    , hotOnly: true
-    , historyApiFallback: true
-  }
-  , plugins: [ new webpack.HotModuleReplacementPlugin() ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 8080,
+    publicPath: "http://localhost:3000/",
+    hotOnly: true,
+    historyApiFallback: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
